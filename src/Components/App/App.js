b4d717d27dd08795c896159b351227f96e2c1af8
Duard <vegasduard@gmail.com>
2022-03-06 14:06:10 -0700

    can now add tracks to playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,13 +9,67 @@ class App extends React.Component {
     super(props);
     this.state = {
       SearchResults: [
-        {name: 'Raining Blood', artist: 'Slayer', album: 'Reign in Blood', id: 1},
-        {name: 'Walk With Me In Hell', artist: 'Lamb of God', album: 'Sacrament', id: 2},
-        {name: 'Damage Done ', artist: 'Dark Tranquility', album: 'Damage Done', id: 3},
-        {name: 'Cold Like War', artist: 'We Came As Romans', album: 'Cold Like War', id: 4}
-      ]
-    }
+        {
+          name: 'Raining Blood',
+          artist: 'Slayer',
+          album: 'Reign in Blood',
+          id: 1,
+        },
+        {
+          name: 'Walk With Me In Hell',
+          artist: 'Lamb of God',
+          album: 'Sacrament',
+          id: 2,
+        },
+        {
+          name: 'Damage Done ',
+          artist: 'Dark Tranquility',
+          album: 'Damage Done',
+          id: 3,
+        },
+        {
+          name: 'Cold Like War',
+          artist: 'We Came As Romans',
+          album: 'Cold Like War',
+          id: 4,
+        },
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+        {
+          name: 'playlistName1',
+          artist: 'playlistArtist1',
+          album: 'playlistAlbum1',
+          id: 5,
+        },
+        {
+          name: 'playlistName2',
+          artist: 'playlistArtist2',
+          album: 'playlistAlbum2',
+          id: 6,
+        },
+        {
+          name: 'playlistName3',
+          artist: 'playlistArtist3',
+          album: 'playlistAlbum3',
+          id: 7,
+        },
+      ],
+    };
+
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -25,8 +79,14 @@ class App extends React.Component {
         <div className='App'>
           <SearchBar />
           <div className='App-playlist'>
-            <SearchResults  searchResults={this.state.SearchResults} />
-            <Playlist />
+            <SearchResults
+              searchResults={this.state.SearchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -4,16 +4,15 @@ import Track from '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
-    return(
-      <div className="TrackList">
-        {
-          this.props.tracks.map(track => {
-            return <Track track={track} 
-                          key={track.id} />
-          })
-        }
+    return (
+      <div className='TrackList'>
+        {this.props.tracks.map((track) => {
+          return (
+            <Track track={track} key={track.id} onAdd={this.props.onAdd} />
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
